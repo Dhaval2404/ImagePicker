d473d1944c5400054104ae3649debf3b0f704a7a
douglas-srs <douglas.srs12@gmail.com>
2019-05-06 10:46:09 -0300

    Clean unused imports

--- imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/ImagePicker.kt
+++ imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/ImagePicker.kt
@@ -3,7 +3,6 @@ package com.github.dhaval2404.imagepicker
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
