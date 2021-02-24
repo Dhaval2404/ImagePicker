c45dd9756eebb2cf3da9d222115617717961563e
Ali Bagheri <a.bagheri@bazarshar.com>
2021-02-24 11:02:23 +0330

    Fix compatibility with api > 17

--- sample/src/main/kotlin/com.github.dhaval2404.imagepicker/sample/MainActivity.kt
+++ sample/src/main/kotlin/com.github.dhaval2404.imagepicker/sample/MainActivity.kt
@@ -49,8 +49,8 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
             R.id.action_github -> {
                 IntentUtil.openURL(this, GITHUB_REPOSITORY)
                 return true
