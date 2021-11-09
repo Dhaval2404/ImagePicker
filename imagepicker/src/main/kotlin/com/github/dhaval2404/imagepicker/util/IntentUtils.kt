53e08ef30c35479f9bf00f25ff9f2f47b31bfcce
Shabaz Ahmed <shabaz1729@gmail.com>
2021-11-09 19:41:52 +0530

    changed intent from ACTION_GET_CONTENT to ACTION_GET_CONTENT

--- imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/util/IntentUtils.kt
+++ imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/util/IntentUtils.kt
@@ -40,7 +40,7 @@ object IntentUtils {
      */
     private fun getGalleryDocumentIntent(mimeTypes: Array<String>): Intent {
         // Show Document Intent
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).applyImageTypes(mimeTypes)
+        val intent = Intent(Intent.ACTION_GET_CONTENT).applyImageTypes(mimeTypes)
         intent.addCategory(Intent.CATEGORY_OPENABLE)
         intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
