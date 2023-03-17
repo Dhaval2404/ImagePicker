db76917cae03c926d6878f2d041f08580f351050
edwnmrtnz <dev.edwinmartinez@gmail.com>
2023-03-17 13:34:20 +0800

    Use path when saving on save instance state

--- imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/provider/CameraProvider.kt
+++ imagepicker/src/main/kotlin/com/github/dhaval2404/imagepicker/provider/CameraProvider.kt
@@ -71,7 +71,8 @@ class CameraProvider(activity: ImagePickerActivity) : BaseProvider(activity) {
      **/
     override fun onSaveInstanceState(outState: Bundle) {
         // Save Camera File
-        outState.putSerializable(STATE_CAMERA_FILE, mCameraFile)
+        //outState.putSerializable(STATE_CAMERA_FILE, mCameraFile.canonicalPath)
+        outState.putString(STATE_CAMERA_FILE, mCameraFile?.canonicalPath)
     }
 
     /**
@@ -79,7 +80,9 @@ class CameraProvider(activity: ImagePickerActivity) : BaseProvider(activity) {
      */
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         // Restore Camera File
-        mCameraFile = savedInstanceState?.getSerializable(STATE_CAMERA_FILE) as File?
+       // mCameraFile = savedInstanceState?.getSerializable(STATE_CAMERA_FILE) as File?
+        val canonicalPath = savedInstanceState?.getString(STATE_CAMERA_FILE) ?: return
+        mCameraFile = getFileDir(canonicalPath)
     }
 
     /**
